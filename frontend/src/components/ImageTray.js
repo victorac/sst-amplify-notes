c98198a338a358d131f6c042084ace2c8b7631b6
Victor <victorde.ac@gmail.com>
2022-11-07 10:43:06 -0300

    fix cropper and edit image

--- frontend/src/components/ImageTray.js
+++ frontend/src/components/ImageTray.js
@@ -1,20 +1,18 @@
 import React, { useState, useRef, useEffect } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Button, ButtonGroup, Container, Image as RBImage, Row } from 'react-bootstrap';
+import Cropper from 'cropperjs';
+import "./ImageTray.css";
+import "cropperjs/dist/cropper.css";
 
-// {
-//     index: {
-//         image:
-//         setImage:
-//     }
-// }
+
 export default function ImageTray({ images, setImages }) {
     const [selectedImage, setSelectedImage] = useState(Object.keys(images)[0] ?? null);
     const imageList = Object.entries(images).map((entry, index) => {
         const entryIndex = entry[0]
         const element = entry[1]
         return (
-            <ListGroup.Item style={{ cursor: "pointer", minWidth: "25%", minHeigth: "25%" }} key={index} onClick={() => setSelectedImage(entryIndex)}>
+            <ListGroup.Item style={{ cursor: "pointer", minWidth: "25%", minHeigth: "25%" }} key={index} onClick={() => selectedImage === entryIndex ? setSelectedImage(null) : setSelectedImage(entryIndex)}>
                 <RBImage width="100%" height="100%" id={entryIndex} src={element.image} />
             </ListGroup.Item>
         );
@@ -24,46 +22,53 @@ export default function ImageTray({ images, setImages }) {
         setSelectedImage(Object.keys(images)[0] ?? null);
         setImages({ ...images });
     }
-    const canvasRef = useRef(null);
     const imageRef = useRef(null);
-    function drawImage(event) {
-        const imageRef = event.target;
-        setImageDimensions({
-            width: imageRef.width,
-            height: imageRef.height
-        });
-        const ctx = canvasRef.current.getContext("2d");
-        ctx.clearRect(0, 0, imageRef.width, imageRef.height);
-        ctx.drawImage(imageRef, 0, 0,);
-    }
-    function redraw(imageRef) {
-        setImageDimensions({
-            width: imageRef.width,
-            height: imageRef.height
-        });
-        const ctx = canvasRef.current.getContext("2d");
-        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-        ctx.drawImage(imageRef, 0, 0,);
-    }
+    const [cropper, setCropper] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
     useEffect(() => {
-        if (canvasRef.current && imageRef.current) {
-            redraw(imageRef.current);
+        cropper?.clear();
+        cropper?.destroy();
+        if (imageRef.current && isEditing) {
+            setCropper(new Cropper(imageRef.current, {
+                viewMode: 3,
+            }));
         }
-    }, [selectedImage]);
-    const [imageDimesions, setImageDimensions] = useState({
-        width: 0,
-        height: 0
-    });
+    }, [isEditing]);
+    function handleCrop() {
+        const canvas = cropper?.getCroppedCanvas();
+        const data = canvas.toDataURL();
+        images[selectedImage].image = data;
+        setImages({ ...images });
+        setIsEditing(false);
+    }
     return (
-        <Container>
+        <Container style={{ maxHeight: "50%" }}>
             <Row className="position-relative">
-                <RBImage ref={imageRef} src={images[selectedImage]?.image} style={{ display: "none" }} onLoad={drawImage} />
-                <canvas width={imageDimesions.width} height={imageDimesions.height} className="p-0 m-0" ref={canvasRef}></canvas>
                 {selectedImage &&
-                    <ButtonGroup className="p-0 m-0 position-absolute" style={{ bottom: 0 }}>
-                        <Button variant="primary">Crop</Button>
-                        <Button variant="danger" onClick={handleRemove}>Remove</Button>
-                    </ButtonGroup>}
+                    <>
+                        <div className="p-0 m-0" style={{ "maxWidth": "100%", "maxHeight": "100%" }}>
+                            <RBImage ref={imageRef} src={images[selectedImage]?.image} />
+                        </div>
+                        <ButtonGroup className="p-0 m-0" style={{ "maxWidth": "100%", "maxHeight": "100%" }}>
+                            {
+                                isEditing ?
+                                    <>
+                                        <Button variant="secondary" onClick={() => cropper?.rotate(-5)}>Rotate Left</Button>
+                                        <Button variant="primary" onClick={handleCrop}>Crop</Button>
+                                        <Button variant="secondary" onClick={() => cropper?.reset()}>Reset</Button>
+                                        <Button variant="danger" onClick={() => setIsEditing(false)}>Cancel Edit</Button>
+                                        <Button variant="secondary" onClick={() => cropper?.rotate(5)}>Rotate Right</Button>
+                                    </>
+                                    :
+                                    <>
+                                        <Button variant="primary" onClick={() => setIsEditing(true)}>Edit</Button>
+                                        <Button variant="danger" onClick={handleRemove}>Remove</Button>
+                                    </>
+                            }
+                        </ButtonGroup>
+                    </>
+
+                }
             </Row>
             <Row className="justify-content-md-center" style={{ maxHeight: "10%" }}>
                 <ListGroup style={{ "overflowX": "auto", "maxWidth": "100%", "maxHeight": "25%" }} flush="true" horizontal>
